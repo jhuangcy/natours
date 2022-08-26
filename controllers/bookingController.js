f4bd8d38ee2c936d1c5226d205543707fe369775
jhuangcy <jcyh87@gmail.com>
2022-08-26 12:52:47 -0600

    Fix mistyped variable

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -70,7 +70,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) =>
 const createBookingCheckout = async session =>
 {
     const tour = session.client_reference_id
-    const user = (await User.findOne({email: session.customer_email})).id
+    const user = (await User.findOne({email: session.customer_email}))._id
     // const price = session.display_items[0].amount / 100
     const price = session.amount_total / 100
 
@@ -93,7 +93,7 @@ exports.webhookCheckout = (req, res, next) =>
         return res.status(400).send(`Webhook error: ${error.message}`)
     }
 
-    if (eveny.type === 'checkout.session.completed') 
+    if (event.type === 'checkout.session.completed') 
         createBookingCheckout(event.data.object)
 
     // Send success response to stripe
